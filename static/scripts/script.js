51220420a84afdc5a99fa9f0ebefd988a93568dd
RondoYummy <basrers@ukr.net>
2025-01-15 23:21:06 +0200

    update SW

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/static/scripts/service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((err) => {
+      console.log('Service Worker registration failed:', err);
+    });
+}
+
 document.getElementById('refreshApp').addEventListener('click', async () => {
   console.log('Refreshing app...');
   await refreshApp();
--- static/scripts/service-worker.js
+++ static/scripts/service-worker.js
@@ -15,17 +15,6 @@ const urlsToCache = [
   '/static/icons/favicon-32x32.ico',
 ];
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/static/service-worker.js')
-    .then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch((err) => {
-      console.log('Service Worker registration failed:', err);
-    });
-}
-
 // Установка сервісного воркера і кешування файлів
 self.addEventListener('install', (event) => {
   event.waitUntil(
