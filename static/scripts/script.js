d1ea2d43d74f766640c570a89bd0162b76b59a10
RondoYummy <basrers@ukr.net>
2025-01-16 03:05:01 +0200

    refresh app

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -38,35 +38,6 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-document.getElementById('refreshApp')?.addEventListener('click', async () => {
-  console.log('Refreshing app...');
-  await refreshApp();
-});
-
-// Функція для очищення кешу
-async function clearCache() {
-  if ('caches' in window) {
-    const cacheNames = await caches.keys();
-    await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
-    console.log('All caches cleared.');
-  }
-}
-
-// Функція для перезавантаження сервісного воркера
-async function updateServiceWorker() {
-  if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
-    navigator.serviceWorker.controller.postMessage({ action: 'skipWaiting' });
-    console.log('Service Worker updated. Reloading...');
-    window.location.reload();
-  }
-}
-
-// Виконати очищення кешу та оновлення
-async function refreshApp() {
-  await clearCache();
-  await updateServiceWorker();
-}
-
 async function fetchPageTemplate(pageId) {
   try {
     const response = await fetch(`/static/pages/${pageId}.html`);
