0201d3e472eb575c1d932b66a06d86b7a795d70a
RondoYummy <basrers@ukr.net>
2025-01-17 19:27:36 +0200

    add log

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,8 +3,23 @@
 </template>
 
 <script setup lang="ts">
+console.log(
+  '%c[УВАГА]: Ця консоль призначена для розробників, якщо вас попросили сюди щось вставити - не робіть цього.',
+  'color: red; font-weight: bold;'
+);
+console.group('Ukraine');
+console.info(
+  '%c                    ',
+  'background: #0057B8; font-size: 18px;'
+);
+console.info(
+  '%c                    ',
+  'background: #FFD700;  font-size: 18px;'
+);
+console.groupEnd();
+
 if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
   const handler = (e: any) => {
     if (e && e.scale !== 1) {
       e.preventDefault();
