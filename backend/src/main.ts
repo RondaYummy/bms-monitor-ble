a66166b434a1f354e22af4b2d943bb537bf5ad28
RondoYummy <basrers@ukr.net>
2025-01-17 16:35:59 +0200

    upd

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.disable('x-powered-by').disable('etag');
+
+  app.useGlobalPipes(
+    new ValidationPipe({ forbidNonWhitelisted: true, whitelist: true }),
+  );
+
+  await app.listen(3010);
+
+  console.log(`🟢 Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
