30cf528c7e9748318cc5ce80bc9aeccf0de0c32e
RondoYummy <basrers@ukr.net>
2025-01-19 17:37:01 +0200

    2

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ def parse_cell_info(data, device_name):
 
         # Extract cell resistances
         cell_resistances = []
-        start_index = 78
+        start_index = 80
         for i in range(num_cells):
             resistance_raw = int.from_bytes(data[start_index:start_index + 2], byteorder='little')
             resistance = resistance_raw * 0.001  # Conversion of resistance
