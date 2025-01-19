6dd00bbfcae09254b9ec020473a0a8370031566b
RondoYummy <basrers@ukr.net>
2025-01-19 21:47:53 +0200

    3

--- main.py
+++ main.py
@@ -206,10 +206,10 @@ def parse_cell_info(data, device_name):
         average_voltage = sum(filtered_voltages) / len(filtered_voltages)
         voltage_diff = max(filtered_voltages) - min(filtered_voltages)
         
-        raw_bytes = data[118:126]
-        log(device_name, f"Raw Bytes Before Parsing: {raw_bytes.hex()}")
-        log(device_name, f"Little-Endian Value: {int.from_bytes(data[118+4:122+4], byteorder='little')}")
+        log(device_name, f"Raw Bytes Before Parsing: {data[118:122].hex()}")
+        log(device_name, f"Little-Endian Value: {int.from_bytes(data[118:122], byteorder='little')}")
         log(device_name, data)
+        log(device_name, data[118:122])
 
         cell_info = {
             "charging_status": charging_status,
