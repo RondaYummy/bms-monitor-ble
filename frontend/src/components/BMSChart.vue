21c767b90a938f91bbaf9d62db5db724241e8a97
RondoYummy <basrers@ukr.net>
2025-01-23 15:24:14 +0200

    tooltip color

--- frontend/src/components/BMSChart.vue
+++ frontend/src/components/BMSChart.vue
@@ -49,6 +49,7 @@ const chartOptions = ref({
   ],
   tooltip: {
     x: { format: 'dd MMM yyyy HH:mm:ss' }, // Формат тултіпа
+    background: '#1e1f26',
   },
   colors: ['#FF4560', '#008FFB', '#F2C037'], // Кольори серій
 });
