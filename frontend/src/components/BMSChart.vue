6d56fb62887271bb1fb93dc2eb0fe30d475d4a04
RondoYummy <basrers@ukr.net>
2025-01-24 18:00:59 +0200

    3

--- frontend/src/components/BMSChart.vue
+++ frontend/src/components/BMSChart.vue
@@ -99,6 +99,14 @@ const chartOptions = ref({
   ],
   tooltip: {
     x: { format: 'dd MMM yyyy HH:mm:ss' },
+    y: [
+      {
+        formatter: (val: number) => `${val} A`
+      },
+      {
+        formatter: (val: number) => `${val} W`
+      },
+    ],
     theme: 'dark',
   },
   // colors: ['#FF4560', '#008FFB', '#F2C037'],
