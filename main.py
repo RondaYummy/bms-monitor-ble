3d71d6cb1781d44f90439ff69b2ba17b309c23c2
RondoYummy <basrers@ukr.net>
2025-01-25 18:14:12 +0200

    dddd

--- main.py
+++ main.py
@@ -302,7 +302,7 @@ async def parse_cell_info(data, device_name, device_address):
         log(device_name, f"Error parsing Cell Info Frame: {e}", force=True)
         return None
 
-async def notification_handler(device, data, device_name, device_address, client):
+async def notification_handler(device, data, device_name, device_address):
     if data[:4] == b'\x55\xAA\xEB\x90':  # The beginning of a new frame
         await device_data_store.clear_buffer(device_name)  # Очистити буфер
     await device_data_store.append_to_buffer(device_name, data)  # Додати нові дані
@@ -327,10 +327,6 @@ async def notification_handler(device, data, device_name, device_address, client
             await parse_cell_info(buffer, device_name, device_address)
         elif frame_type == 0x01:
             await parse_setting_info(buffer, device_name, device_address)
-            await client.disconnect()
-            device_info_data = await device_data_store.get_device_info(device.name)
-            device_info_data["connected"] = False
-            await device_data_store.update_device_info(device.name, device_info_data)
         else:
             log(device_name, f"Unknown frame type {frame_type}: {buffer}", force=True)
             # Якщо невідомий тип очищую буфер, бо така помилка буде весь час падати
@@ -354,7 +350,7 @@ async def connect_and_run(device):
                 log(device.name, f"Connected and notification started", force=True)
 
                 def handle_notification(sender, data):
-                    asyncio.create_task(notification_handler(device, data, device.name, device.address, client))
+                    asyncio.create_task(notification_handler(device, data, device.name, device.address))
 
                 await client.start_notify(CHARACTERISTIC_UUID, handle_notification)
 
@@ -385,6 +381,7 @@ async def connect_and_run(device):
             log(device.name, f"Error: {str(e)}", force=True)
         finally:
             device_info_data["connected"] = False
+            await client.disconnect()
             await device_data_store.update_device_info(device.name, device_info_data)
             log(device.name, "Disconnected, retrying in 5 seconds...", force=True)
             await asyncio.sleep(5)
