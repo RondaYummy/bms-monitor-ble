69b7441c1c64442adaf3f032126563b671c7b77c
RondoYummy <basrers@ukr.net>
2025-01-25 22:07:27 +0200

    333

--- main.py
+++ main.py
@@ -427,6 +427,8 @@ async def are_all_allowed_devices_connected_and_have_data() -> bool:
         for device_info in connected_devices.values()
         if device_info.get("connected", False)
     }
+    log("ALLOWED DEVICES", f"[{allowed_devices}]", force=True)
+    log("CONNECTED DEVICES", f"[{connected_addresses}]", force=True)
 
     # Перевіряємо, чи всі дозволені пристрої підключені
     if not allowed_devices.issubset(connected_addresses):
@@ -435,10 +437,10 @@ async def are_all_allowed_devices_connected_and_have_data() -> bool:
 
     # Перевіряємо, чи є дані cell_info для кожного підключеного пристрою
     cell_info = await device_data_store.get_cell_info()
+    log("CELL INFO", f"[{cell_info}]", force=True)
     for device_address in allowed_devices:
         if device_address not in cell_info:
             log("CHECK DEVICES", f"Device [{device_address}] have no data.", force=True)
-            log("CHECK DEVICES", f"[{cell_info}]", force=True)
             return False
 
     return True
