131b6e468e5e76979915bf411b9831dee0fd6a02
RondoYummy <basrers@ukr.net>
2025-01-26 13:19:03 +0200

    3

--- frontend/src/components/BMSChart.vue
+++ frontend/src/components/BMSChart.vue
@@ -74,6 +74,12 @@ const chartOptions = ref({
       style: {
         colors: "#aaa"
       },
+      formatter: function (value) {
+        const date = new Date(value);
+        const offset = date.getTimezoneOffset();
+        const localDate = new Date(date.getTime() - offset * 60 * 1000);
+        return `${localDate.toISOString().slice(0, 16)}`;
+      }
       // formatter: function (value) {
       //   const date = new Date(value);
       //   const offset = date.getTimezoneOffset();
