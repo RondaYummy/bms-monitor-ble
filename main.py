3fe01bb8d027a8ae36a1d110a43c8b35369e286d
RondoYummy <basrers@ukr.net>
2025-01-27 12:14:13 +0200

    3

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ async def get_device_info():
                 "timestamp": alert[4],
                 "level": "",
             }
-        error_code = alert.get("error_code")
+        error_code = int(alert.get("error_code"))
         message = error_codes.get(error_code, {}).get('message', 'Message not found')
         level = error_codes.get(error_code, {}).get('level', 'Level not found')
         enriched_alert = {**alert, "message": message, "level": level}
