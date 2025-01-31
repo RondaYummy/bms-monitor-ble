51dcf55c11196262a56587abc16a7e7ebbbb356b
RondoYummy <basrers@ukr.net>
2025-01-31 12:10:37 +0200

    Refactor BMS parsing and data storage into separate modules
    - Move DataStore class from main.py to a new data_store.py module
    - Extract parsing functions from main.py to a new jk_bms_parser.py module
    - Remove redundant imports and centralize data storage logic
    - Improve code organization and modularity

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import asyncio
-from asyncio import Lock
 from datetime import datetime
-from copy import deepcopy
 import os
 import yaml
 from uuid import uuid4
@@ -21,11 +19,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from pydantic import BaseModel
 from fastapi import Body
-from async_timeout import timeout
 
 from python.colors import *
 import python.db as db
-import python.battery_alerts as alerts
+from python.jk_bms_parser import parse_device_info, parse_setting_info
+from python.data_store import data_store
 
 with open('configs/error_codes.yaml', 'r') as file:
     error_codes = yaml.safe_load(file)
@@ -44,73 +42,6 @@ JK_BMS_OUI = {"c8:47:80"} # Через кому можна додати усі �
 
 PASSWORD = "123456"
 TOKEN_LIFETIME_SECONDS = 3600
-
-# Data store class
-class DataStore:
-    def __init__(self):
-        self.device_info = {}
-        self.cell_info = {}
-        self.last_cell_info_update = {}
-        self.response_buffers = {}
-        self.active_tokens = {}
-        self.lock = Lock()
-
-    async def add_token(self, token: str, device_name: str):
-        async with self.lock:
-            self.active_tokens[token] = {"device_name": device_name}
-
-    async def remove_token(self, token: str):
-        async with self.lock:
-            if token in self.active_tokens:
-                del self.active_tokens[token]
-
-    async def is_token_valid(self, token: str) -> bool:
-        async with self.lock:
-            return token in self.active_tokens
-
-    async def update_last_cell_info_update(self, device_name):
-        async with self.lock:
-            self.last_cell_info_update[device_name] = datetime.now()
-
-    async def get_last_cell_info_update(self, device_name):
-        async with self.lock:
-            return self.last_cell_info_update.get(device_name, None)
-        
-    async def append_to_buffer(self, device_name, data):
-        async with self.lock:
-            if device_name not in self.response_buffers:
-                self.response_buffers[device_name] = bytearray()
-            self.response_buffers[device_name].extend(data)
-
-    async def get_buffer(self, device_name):
-        async with self.lock:
-            return self.response_buffers.get(device_name, bytearray())
-
-    async def clear_buffer(self, device_name):
-        async with self.lock:
-            if device_name in self.response_buffers:
-                self.response_buffers[device_name].clear()
-
-    async def update_device_info(self, device_name, info):
-        async with self.lock:
-            self.device_info[device_name] = info
-
-    async def get_device_info(self, device_name=None):
-        async with self.lock:
-            if device_name:
-                return deepcopy(self.device_info.get(device_name))
-            return deepcopy(self.device_info)
-
-    async def update_cell_info(self, device_name, info):
-        async with self.lock:
-            self.cell_info[device_name] = info
-
-    async def get_cell_info(self):
-        async with self.lock:
-            return deepcopy(self.cell_info)
-
-# Initialize data store
-data_store = DataStore()
 
 app = FastAPI()
 auth_scheme = HTTPBearer()
@@ -313,176 +244,6 @@ def log(device_name, message, force=False):
     if ENABLE_LOGS or force:
         current_time = datetime.now().strftime("%d.%m.%y %H:%M")
         print(f"{BLUE}[{device_name}] {MAGENTA}[{current_time}]{RESET} {message}")
-
-async def parse_device_info(data, device_name, device_address):
-    """Parsing Device Info Frame (0x03)."""
-    log(device_name, "Parsing Device Info Frame...")
-
-    try:
-        log(device_name, f"Raw data: {data.hex()}")
-        
-        # Checking the header
-        if data[:4] != b'\x55\xAA\xEB\x90':
-            raise ValueError("Invalid frame header")
-
-        # Extract fields from the frame
-        device_info = {
-            # "setup_passcode": data[118:134].split(b'\x00', 1)[0].decode('utf-8', errors='ignore'),
-            # "passcode": data[97:102].split(b'\x00', 1)[0].decode('utf-8', errors='ignore'),
-            # "device_passcode": data[62:78].split(b'\x00', 1)[0].decode('utf-8', errors='ignore'),
-            "frame_type": data[4],
-            "frame_counter": data[5],
-            "vendor_id": data[6:22].split(b'\x00', 1)[0].decode('utf-8', errors='ignore'),
-            "hardware_version": data[22:30].split(b'\x00', 1)[0].decode('utf-8', errors='ignore'),
-            "software_version": data[30:38].split(b'\x00', 1)[0].decode('utf-8', errors='ignore'),
-            "device_uptime": int.from_bytes(data[38:42], byteorder='little'),
-            "power_on_count": int.from_bytes(data[42:46], byteorder='little'),
-            "device_name": data[46:62].split(b'\x00', 1)[0].decode('utf-8', errors='ignore'),
-            "device_address": device_address,
-            "manufacturing_date": data[78:86].split(b'\x00', 1)[0].decode('utf-8', errors='ignore'),
-            "serial_number": data[86:97].split(b'\x00', 1)[0].decode('utf-8', errors='ignore'),
-            "user_data": data[102:118].split(b'\x00', 1)[0].decode('utf-8', errors='ignore'),
-            "connected": True,
-        }
-
-        # CRC Validation
-        crc_calculated = calculate_crc(data[:-1])
-        crc_received = data[-1]
-        if crc_calculated != crc_received:
-            log(device_name, f"Invalid CRC: {crc_calculated} != {crc_received}")
-            return None
-        
-        # Save device-specific info
-        await data_store.update_device_info(device_name, device_info)
-
-        # Logging of parsed information
-        log(device_name, "Parsed Device Info:")
-        for key, value in device_info.items():
-            log(device_name, f"{key}: {value}")
-
-        return device_info
-
-    except Exception as e:
-        log(device_name, f"Error parsing Device Info Frame: {e}", force=True)
-        return None
-    
-async def parse_setting_info(data, device_name, device_address):
-    """Parsing Cell Info Frame (0x01)."""
-    log(device_name, "Parsing Setting Info Frame...")  
-
-    try:
-        log(device_name, f"Setting Header: {data[:5].hex()}", force=True)
-
-    except Exception as e:
-        log(device_name, f"Error parsing Setting Info Frame: {e}", force=True)
-        return None
-
-async def parse_cell_info(data, device_name, device_address):
-    """Parsing Cell Info Frame (0x02)."""
-    log(device_name, "Parsing Cell Info Frame...")
-
-    try:
-        # Checking the header
-        if data[:4] != b'\x55\xAA\xEB\x90':
-            log(device_name, f"Unexpected Header: {data[:4].hex()}", force=True)
-            raise ValueError("Invalid frame header")
-        log(device_name, f"Data Length: {len(data)}")
-
-        # Extract cell data
-        cell_voltages = []
-        start_index = 6  # Initial index for cell tension
-        num_cells = 32   # Maximum number of cells
-        for i in range(num_cells):
-            voltage_raw = int.from_bytes(data[start_index:start_index + 2], byteorder='little')
-            voltage = voltage_raw * 0.001  # Conversion of volts
-            cell_voltages.append(voltage)
-            start_index += 2
-
-        # Extract cell resistances
-        cell_resistances = []
-        start_index = 80
-        for i in range(num_cells):
-            resistance_raw = int.from_bytes(data[start_index:start_index + 2], byteorder='little')
-            resistance = resistance_raw * 0.001  # Conversion of resistance
-            cell_resistances.append(resistance)
-            start_index += 2
-
-        # Filter only those cells that have a voltage > 0
-        filtered_voltages = [v for v in cell_voltages if v > 0]
-        filtered_resistances = [v for v in cell_resistances if v > 0]
-
-        # TODO need add 32 to pos, after cell resistance?
-        power_tube_temp = int.from_bytes(data[112+32:114+32], byteorder='little', signed=True) * 0.1
-        battery_voltage = int.from_bytes(data[118+32:122+32], byteorder='little', signed=True) * 0.001
-        battery_power = int.from_bytes(data[122+32:126+32], byteorder='little', signed=True) * 0.001
-        charge_current = int.from_bytes(data[126+32:130+32], byteorder='little', signed=True) * 0.001
-        temperature_sensor_1 = int.from_bytes(data[130+32:132+32], byteorder='little', signed=True) * 0.1
-        temperature_sensor_2 = int.from_bytes(data[132+32:134+32], byteorder='little', signed=True) * 0.1
-
-        state_of_charge = data[141+32]
-        remaining_capacity = int.from_bytes(data[142+32:146+32], byteorder='little') * 0.001
-        nominal_capacity = int.from_bytes(data[146+32:150+32], byteorder='little') * 0.001
-        cycle_count = int.from_bytes(data[150+32:154+32], byteorder='little')
-        total_cycle_capacity = int.from_bytes(data[154+32:157+32], byteorder='little') * 0.001
-        state_of_health = data[158+32]
-        charging_status = data[166+32]
-        discharging_status = data[167+32]
-        precharging_status = data[168+32]
-
-        temperature_sensor_5 = int.from_bytes(data[222+32:224+32], byteorder='little', signed=True) * 0.1
-        temperature_sensor_4 = int.from_bytes(data[224+32:226+32], byteorder='little', signed=True) * 0.1
-        temperature_sensor_3 = int.from_bytes(data[226+32:228+32], byteorder='little', signed=True) * 0.1
-        emergency_time_countdown = int.from_bytes(data[186+32:187+32], byteorder='little')
-
-        average_voltage = sum(filtered_voltages) / len(filtered_voltages)
-        voltage_diff = max(filtered_voltages) - min(filtered_voltages)
-        
-        cell_info = {
-            "device_address": device_address.lower(),
-            "charging_status": charging_status,
-            "discharging_status": discharging_status,
-            "precharging_status": precharging_status,
-            "voltage_difference": voltage_diff,
-            "average_voltage": average_voltage,
-            "cell_voltages": filtered_voltages,
-            "cell_resistances": filtered_resistances,
-            "power_tube_temperature": power_tube_temp,
-            "battery_voltage": battery_voltage,
-            "battery_power": battery_power,
-            "charge_current": charge_current,
-            "temperature_sensor_1": temperature_sensor_1,
-            "temperature_sensor_2": temperature_sensor_2,
-            "temperature_sensor_3": temperature_sensor_3,
-            "temperature_sensor_4": temperature_sensor_4,
-            "temperature_sensor_5": temperature_sensor_5,
-            "state_of_charge": state_of_charge,
-            "remaining_capacity": remaining_capacity,
-            "nominal_capacity": nominal_capacity,
-            "cycle_count": cycle_count,
-            "total_cycle_capacity": total_cycle_capacity,
-            "state_of_health": state_of_health,
-            "emergency_time_countdown": emergency_time_countdown,
-        }
-
-        # CRC Validation
-        crc_calculated = calculate_crc(data[:-1])
-        crc_received = data[-1]
-        if crc_calculated != crc_received:
-            log(device_name, f"Invalid CRC: {crc_calculated} != {crc_received}")
-            return None
-        
-        await data_store.update_cell_info(device_name, cell_info)
-        await alerts.evaluate_alerts(device_address=device_address, device_name=device_name, cell_info=cell_info)
-
-        if await are_all_allowed_devices_connected_and_have_data():
-            db.update_aggregated_data(device_name=device_name, device_address=device_address, current=charge_current, power=battery_power)
-
-        log(device_name, "Parsed Cell Info.")
-        return cell_info
-
-    except Exception as e:
-        log(device_name, f"Error parsing Cell Info Frame: {e}", force=True)
-        return None
 
 async def notification_handler(device, data, device_name, device_address):
     if data[:4] == b'\x55\xAA\xEB\x90':  # The beginning of a new frame
