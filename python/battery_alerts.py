014d2a85374cc261d4b6480210bcd4c67a4b508f
RondoYummy <basrers@ukr.net>
2025-02-02 23:24:55 +0200

    alerts

--- python/battery_alerts.py
+++ python/battery_alerts.py
@@ -123,8 +123,8 @@ async def evaluate_alerts(device_address: str, device_name: str, cell_info: Cell
         elif cell_info["emergency_time_countdown"] < 10:
             add_alert(alerts, "1025")
 
+        print(f"Alerts: {alerts}")
         for alert in alerts:
-            print(f"alert: {alert}")
             db.insert_alert_data(device_address, device_name, alert['id'], datetime.now(), config['alerts']['n_hours'])
             await send_push_notifications(device_name, {"id": alert['id'], "message": error_codes[alert['id']]["message"]})
 
