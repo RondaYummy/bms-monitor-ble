60f464fda903c180257aaacb7046f46e4f98e369
RondoYummy <basrers@ukr.net>
2025-02-02 21:11:32 +0200

    3

--- frontend/src/composables/usePush.ts
+++ frontend/src/composables/usePush.ts
@@ -22,7 +22,7 @@ export function usePush() {
       console.log("Push Subscription отримано:", subscription);
 
       // Відправити підписку на сервер
-      await fetch("https://your-backend.com/save-subscription", {
+      await fetch("/save-subscription", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(subscription),
@@ -38,7 +38,7 @@ export function usePush() {
 // Функція перетворення ключа VAPID у Uint8Array
 function urlBase64ToUint8Array(base64String: string): Uint8Array {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/");
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
   const rawData = atob(base64);
   return new Uint8Array([...rawData].map((char) => char.charCodeAt(0)));
 }
