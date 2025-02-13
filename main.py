1afbf70ef4b06fdb4ef59010c1a4e8a7b03ef91f
RondoYummy <basrers@ukr.net>
2025-02-13 20:00:45 +0200

    config

--- main.py
+++ main.py
@@ -62,6 +62,7 @@ async def login(request: Request):
     body = await request.json()
     password = body.get("password", "")
     config = db.get_config()
+    log("LOGIN", f"config: {config}")
     if not config or password != config.get("password"):
         raise HTTPException(status_code=401, detail="Invalid password")
 
