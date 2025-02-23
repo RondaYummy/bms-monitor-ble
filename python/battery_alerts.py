a58683da80021ad90b9b044493bca7dec16832fc
RondoYummy <basrers@ukr.net>
2025-02-23 13:31:11 +0200

    3

--- python/battery_alerts.py
+++ python/battery_alerts.py
@@ -94,9 +94,9 @@ async def evaluate_alerts(device_address: str, device_name: str, cell_info: Cell
         elif max(cell_info["cell_resistances"]) > 0.3:
             add_alert(alerts, "1019")
 
-        if cell_info["battery_voltage"] > 0:
+        if cell_info["battery_voltage"] > 60:
             add_alert(alerts, "1020")
-        elif cell_info["battery_voltage"] > 58:
+        elif cell_info["battery_voltage"] > 0:
             add_alert(alerts, "1021")
         elif cell_info["battery_voltage"] < 40:
             add_alert(alerts, "1022")
