b3a0ae06fb208bcec51a91ae6cddd284a55e03bd
RondoYummy <basrers@ukr.net>
2025-02-27 00:32:50 +0200

    Refactor SettingsPage layout and remove unnecessary div wrapper

--- frontend/src/pages/SettingsPage.vue
+++ frontend/src/pages/SettingsPage.vue
@@ -183,85 +183,83 @@
                          color="orange"
                          inset />
 
-            <div class="column q-mt-md q-mb-md"
-                 v-if="config">
-              <p class='text-caption'>
-                Налаштування ваших сповіщень
-              </p>
-              <q-btn class="q-mt-sm"
-                     @click="alertsModal = true"
-                     color="black"
-                     :disable="!token"
-                     label="Налаштування Alerts" />
-              <AlertsSettingsModal :config="config"
-                                   :show="alertsModal"
-                                   @update:show="(value) => alertsModal = value" />
+            <p class='text-caption'>
+              Налаштування ваших сповіщень
+            </p>
+            <q-btn class="q-mt-sm"
+                   @click="alertsModal = true"
+                   color="black"
+                   :disable="!token"
+                   label="Налаштування Alerts" />
+            <AlertsSettingsModal v-if="config"
+                                 :config="config"
+                                 :show="alertsModal"
+                                 @update:show="(value) => alertsModal = value" />
 
-              <q-separator class="q-mt-md"
-                           color="orange"
-                           inset />
+            <q-separator class="q-mt-md"
+                         color="orange"
+                         inset />
 
-              <p class='text-caption'>
-                Щоб переглянути налаштування вашого JK-BMS, оберіть пристрій.
-              </p>
-              <q-btn-dropdown :disable="!settings?.length"
-                              class="q-mt-sm"
-                              auto-close
-                              stretch
-                              flat
-                              style='flex: 1 1 50%;'
-                              label="Оберіть пристрій">
+            <p class='text-caption'>
+              Щоб переглянути налаштування вашого JK-BMS, оберіть пристрій.
+            </p>
+            <q-btn-dropdown :disable="!settings?.length"
+                            class="q-mt-sm"
+                            auto-close
+                            stretch
+                            flat
+                            style='flex: 1 1 50%;'
+                            label="Оберіть пристрій">
 
-                <q-list v-if="settings?.length">
-                  <q-item clickable
-                          v-for="setting of settings"
-                          class="text-black"
-                          :key="setting?.address"
-                          :name="setting?.name"
-                          :label="setting?.name"
-                          @click="currentSetting = setting">
-                    <q-item-section>{{ setting?.name }}</q-item-section>
-                  </q-item>
-                </q-list>
-              </q-btn-dropdown>
+              <q-list v-if="settings?.length">
+                <q-item clickable
+                        v-for="setting of settings"
+                        class="text-black"
+                        :key="setting?.address"
+                        :name="setting?.name"
+                        :label="setting?.name"
+                        @click="currentSetting = setting">
+                  <q-item-section>{{ setting?.name }}</q-item-section>
+                </q-item>
+              </q-list>
+            </q-btn-dropdown>
 
-              <template v-if="currentSetting">
-                <ToggleButton :value="currentSetting?.charge_switch"
-                              title="Charge" />
-                <ToggleButton :value="currentSetting?.discharge_switch"
-                              title="Discharge" />
-                <ToggleButton :value="currentSetting?.balancer_switch"
-                              title="Balance" />
-                <!-- <ToggleButton :value="currentSetting?."
+            <template v-if="currentSetting">
+              <ToggleButton :value="currentSetting?.charge_switch"
+                            title="Charge" />
+              <ToggleButton :value="currentSetting?.discharge_switch"
+                            title="Discharge" />
+              <ToggleButton :value="currentSetting?.balancer_switch"
+                            title="Balance" />
+              <!-- <ToggleButton :value="currentSetting?."
                             title="Emergency" /> -->
-                <ToggleButton :value="currentSetting?.heating_enabled"
-                              title="Heating" />
-                <ToggleButton :value="currentSetting?.disable_temperature_sensors"
-                              title="Disable Temp. Sensor" />
-                <ToggleButton :value="currentSetting?.display_always_on"
-                              title="Display Always On" />
-                <ToggleButton :value="currentSetting?.special_charger"
-                              title="Special Charger On" />
-                <ToggleButton :value="currentSetting?.smart_sleep"
-                              title="Smart Sleep On" />
-                <ToggleButton :value="currentSetting?.timed_stored_data"
-                              title="Timed Stored Data" />
-                <ToggleButton :value="currentSetting?.charging_float_mode"
-                              title="Charging Float Mode" />
-                <ToggleButton :value="currentSetting?.gps_heartbeat"
-                              title="GPS Heartbeat" />
-                <ToggleButton :value="currentSetting?.disable_pcl_module"
-                              title="Disable PCL Module" />
-                <ToggleButton :value="currentSetting?.charge_utpr"
-                              title="Charge UTPR" />
+              <ToggleButton :value="currentSetting?.heating_enabled"
+                            title="Heating" />
+              <ToggleButton :value="currentSetting?.disable_temperature_sensors"
+                            title="Disable Temp. Sensor" />
+              <ToggleButton :value="currentSetting?.display_always_on"
+                            title="Display Always On" />
+              <ToggleButton :value="currentSetting?.special_charger"
+                            title="Special Charger On" />
+              <ToggleButton :value="currentSetting?.smart_sleep"
+                            title="Smart Sleep On" />
+              <ToggleButton :value="currentSetting?.timed_stored_data"
+                            title="Timed Stored Data" />
+              <ToggleButton :value="currentSetting?.charging_float_mode"
+                            title="Charging Float Mode" />
+              <ToggleButton :value="currentSetting?.gps_heartbeat"
+                            title="GPS Heartbeat" />
+              <ToggleButton :value="currentSetting?.disable_pcl_module"
+                            title="Disable PCL Module" />
+              <ToggleButton :value="currentSetting?.charge_utpr"
+                            title="Charge UTPR" />
 
-                <SettingsList :settings="currentSetting" />
-              </template>
+              <SettingsList :settings="currentSetting" />
+            </template>
 
-              <q-separator class="q-mt-md"
-                           color="orange"
-                           inset />
-            </div>
+            <q-separator class="q-mt-md"
+                         color="orange"
+                         inset />
           </q-tab-panel>
 
           <q-tab-panel name="Devices">
