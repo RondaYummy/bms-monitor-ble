e6e9e0cb115afa99b4f0d6e6c4fbfcdb578afba2
RondoYummy <basrers@ukr.net>
2025-05-23 17:55:24 +0000

    Comment out server reboot commands in deploy script to prevent unintended server restarts during deployment process.

--- deploy.sh
+++ deploy.sh
@@ -55,8 +55,8 @@ function deploy() {
   # END
   echo "====> Оновлення проекту завершено успішно"
 
-  echo "====> Перезапускаємо сервер Ubuntu..."
-  sudo reboot
+  # echo "====> Перезапускаємо сервер Ubuntu..."
+  # sudo reboot
 }
 
 function check_deploy() {
