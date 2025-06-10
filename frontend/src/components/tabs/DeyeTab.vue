dd870a6b7977e384cc08fd711dff453e9bc090dc
RondoYummy <basrers@ukr.net>
2025-06-10 05:53:33 +0000

    Refactor DeyeTab.vue for improved device management functionality
    - Consolidated input fields and buttons for adding new Deye devices to enhance layout consistency.
    - Added delete functionality for Deye devices with user notifications for successful and error states.
    - Improved styling of device items for better visual clarity and user experience.

--- frontend/src/components/tabs/DeyeTab.vue
+++ frontend/src/components/tabs/DeyeTab.vue
@@ -1,54 +1,39 @@
 <template>
   <div class="text-h6 q-mb-sm full-width">Deye inverter Devices</div>
-  <q-expansion-item
-    :disable="!token"
-    v-model="expandAddDeyeDevice"
-    icon="add"
-    label="Add new device"
-    dark
-    dense-toggle
-  >
+  <q-expansion-item :disable="!token" v-model="expandAddDeyeDevice" icon="add" label="Add new device" dark dense-toggle>
     <p>
       Перш ніж додати ваш інвертор Deye, необхідно дізнатися IP-адресу Wi-Fi стіка та серійний номер
       пристрою. Рекомендуємо призначити статичну IP-адресу, щоб уникнути збоїв у роботі.
     </p>
 
-    <q-input
-      label-color="white"
-      label="Device IP Address"
-      :disable="!token"
-      v-model="createDeye.ip"
-      filled
-      class="q-mb-sm q-mt-sm"
-    />
-    <q-input
-      label-color="white"
-      label="Device Serial Number"
-      :disable="!token"
-      v-model="createDeye.serial_number"
-      filled
-      class="q-mb-sm q-mt-sm"
-    />
-    <q-btn
-      :loading="loading"
-      @click="createDeyeDevice"
-      :disable="!token || !createDeye.ip || !createDeye.serial_number"
-      color="black"
-      label="Додати новий Deye пристрій"
-    />
+    <q-input label-color="white" label="Device IP Address" :disable="!token" v-model="createDeye.ip" filled
+      class="q-mb-sm q-mt-sm" />
+    <q-input label-color="white" label="Device Serial Number" :disable="!token" v-model="createDeye.serial_number"
+      filled class="q-mb-sm q-mt-sm" />
+    <q-btn :loading="loading" @click="createDeyeDevice" :disable="!token || !createDeye.ip || !createDeye.serial_number"
+      color="black" label="Додати новий Deye пристрій" />
   </q-expansion-item>
 
   <div v-for="item of deyeStore?.deyeData" :key="item?.serial_number">
-    <div class="deye-item column q-mt-sm q-mb-sm rounded-borders q-pa-sm">
+    <div class="deye-item column q-mt-sm q-mb-sm rounded-borders q-pa-md">
       <div class="row justify-between">
-        <span>ID: {{ item?.id }}</span>
+        <span>
+          <q-icon @click.prevent="deleteDevice(item?.ip)" class="cursor-pointer q-mr-sm" name="delete"
+            size="1.5em"></q-icon>
+          ID: {{ item?.id }}
+        </span>
         <span>IP: {{ item?.ip }}</span>
       </div>
       <div class="row justify-between">
-        <span>Serial Number: {{ item?.serial_number }}</span>
-        <span :class="item?.device_on == 0 ? 'text-red' : 'text-green'"
-          >Device State: {{ item?.device_on == 0 ? 'Disabled' : 'Enabled' }}</span
-        >
+        <span>SN:
+          {{ item?.serial_number }}
+          <q-tooltip>
+            Серійний номер пристрою.
+          </q-tooltip>
+        </span>
+        <span :class="item?.device_on == 0 ? 'text-red' : 'text-green'">
+          {{ item?.device_on == 0 ? 'Disabled' : 'Enabled' }}
+        </span>
       </div>
     </div>
   </div>
@@ -58,6 +43,7 @@
 import { onBeforeUnmount, onMounted, ref } from 'vue';
 import { useSessionStorage } from 'src/helpers/utils';
 import { useDeyeStore } from 'src/stores/deye';
+import { Notify } from 'quasar';
 
 const deyeStore = useDeyeStore();
 
@@ -80,6 +66,25 @@ async function createDeyeDevice() {
     console.error(error);
   } finally {
     loading.value = false;
+  }
+}
+
+async function deleteDevice(ip: string) {
+  try {
+    await deyeStore.deleteDeyeDevice(ip);
+    Notify.create({
+      message: 'Ви успішно видалили пристрій',
+      color: 'secondary',
+    });
+  } catch (error) {
+    console.error(error);
+    Notify.create({
+      message: 'Сталася помилка під час видалення пристрою',
+      color: 'red',
+      icon: 'warning',
+      position: 'top',
+      timeout: 2000,
+    });
   }
 }
 
