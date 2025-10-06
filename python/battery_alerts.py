394fd43a252b98c5a355ea90e91d173d3b275779
RondoYummy <basrers@ukr.net>
2025-10-06 08:42:44 +0000

    Update error codes and battery alert thresholds for improved accuracy
    - Adjusted battery voltage thresholds in error_codes.yaml to reflect new critical and error levels, ensuring timely alerts for battery performance.
    - Updated battery_alerts.py to align with the new voltage thresholds, enhancing the alert system's responsiveness to battery conditions.

--- python/battery_alerts.py
+++ python/battery_alerts.py
@@ -94,13 +94,13 @@ async def evaluate_alerts(device_address: str, device_name: str, cell_info: Cell
         elif max(cell_info["cell_resistances"]) > 0.3:
             add_alert(alerts, "1019")
 
-        if cell_info["battery_voltage"] > 60:
+        if cell_info["battery_voltage"] > 55:
             add_alert(alerts, "1020")
-        elif cell_info["battery_voltage"] > 58:
+        elif cell_info["battery_voltage"] > 51:
             add_alert(alerts, "1021")
-        elif cell_info["battery_voltage"] < 40:
+        elif cell_info["battery_voltage"] < 41:
             add_alert(alerts, "1022")
-        elif cell_info["battery_voltage"] < 44:
+        elif cell_info["battery_voltage"] < 43:
             add_alert(alerts, "1023")
 
         if cell_info["emergency_time_countdown"] < 5:
