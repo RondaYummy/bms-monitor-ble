be47e0b57bd65848cd67cb17119b6045220892a9
RondoYummy <basrers@ukr.net>
2025-10-24 15:00:07 +0000

    Refactor DeyeTab.vue for improved device management UI and styling
    - Simplified the structure of the device addition section for better readability.
    - Enhanced the display of existing devices with improved layout and styling, including muted labels for device information.
    - Updated CSS for a more modern look, including gradient backgrounds and rounded borders for device items.

--- frontend/src/components/tabs/DeyeTab.vue
+++ frontend/src/components/tabs/DeyeTab.vue
@@ -1,66 +1,46 @@
 <template>
   <div class="text-h6 q-mb-sm full-width">Deye inverter Devices</div>
-  <q-expansion-item
-    :disable="!token"
-    v-model="expandAddDeyeDevice"
-    icon="add"
-    label="Add new device"
-    dark
-    dense-toggle
-  >
+  <q-expansion-item :disable="!token" v-model="expandAddDeyeDevice" icon="add" label="Add new device" dark dense-toggle>
     <p>
       Перш ніж додати ваш інвертор Deye, необхідно дізнатися IP-адресу Wi-Fi стіка та серійний номер
       пристрою. Рекомендуємо призначити статичну IP-адресу, щоб уникнути збоїв у роботі.
     </p>
 
-    <q-input
-      label-color="white"
-      label="Device IP Address"
-      :disable="!token"
-      v-model="createDeye.ip"
-      filled
-      class="q-mb-sm q-mt-sm"
-    />
-    <q-input
-      label-color="white"
-      label="Device Serial Number"
-      :disable="!token"
-      v-model="createDeye.serial_number"
-      filled
-      class="q-mb-sm q-mt-sm"
-    />
-    <q-btn
-      :loading="loading"
-      @click="createDeyeDevice"
-      :disable="!token || !createDeye.ip || !createDeye.serial_number"
-      color="black"
-      label="Додати новий Deye пристрій"
-    />
+    <q-input label-color="white" label="Device IP Address" :disable="!token" v-model="createDeye.ip" filled
+      class="q-mb-sm q-mt-sm" />
+    <q-input label-color="white" label="Device Serial Number" :disable="!token" v-model="createDeye.serial_number"
+      filled class="q-mb-sm q-mt-sm" />
+    <q-btn :loading="loading" @click="createDeyeDevice" :disable="!token || !createDeye.ip || !createDeye.serial_number"
+      color="black" label="Додати новий Deye пристрій" />
   </q-expansion-item>
 
   <div v-for="item of deyeStore?.deyeData" :key="item?.serial_number">
-    <div class="deye-item column q-mt-sm q-mb-sm rounded-borders q-pa-md">
-      <div class="row justify-between">
-        <span>
-          <q-icon
-            @click.prevent="deleteDevice(item?.ip)"
-            class="cursor-pointer q-mr-sm"
-            name="delete"
-            size="1.5em"
-          ></q-icon>
-          <span @click="copy(item?.id)"> ID: {{ item?.id }} </span>
-        </span>
-        <span @click="copy(item?.ip)">IP: {{ item?.ip }}</span>
-      </div>
-      <div class="row justify-between">
-        <span @click="copy(item?.serial_number)">
-          SN:
-          {{ item?.serial_number }}
-          <q-tooltip> Серійний номер пристрою. </q-tooltip>
-        </span>
-        <span :class="item?.device_on == 0 ? 'text-red' : 'text-green'">
-          {{ item?.device_on == 0 ? 'Disabled' : 'Enabled' }}
-        </span>
+    <div class="deye-item row q-mt-sm q-mb-sm rounded-borders q-pa-lg">
+      <q-icon @click.prevent="deleteDevice(item?.ip)" class="cursor-pointer q-mr-sm remove" name="delete" size="1.5em"
+        color="red"></q-icon>
+
+      <div class="column" style="flex: 1">
+        <div class="row justify-between">
+          <span @click="copy(item?.id)">
+            <span class="muted">Device ID:</span> {{ item?.id }}
+          </span>
+
+          <span @click="copy(item?.ip)">
+            <span class="muted">IP:</span> {{ item?.ip }}
+          </span>
+        </div>
+
+        <div class="row justify-between">
+          <span @click="copy(item?.serial_number)">
+            <span class="muted">SN:</span>
+            {{ item?.serial_number }}
+            <q-tooltip> Серійний номер пристрою. </q-tooltip>
+          </span>
+
+          <span :class="item?.device_on == 0 ? 'text-red' : 'text-green'">
+            {{ item?.device_on == 0 ? 'Disabled' : 'Enabled' }}
+          </span>
+        </div>
       </div>
     </div>
   </div>
@@ -131,6 +111,18 @@ deyeStore.fetchDeyeDevices();
 
 <style scoped lang="scss">
 .deye-item {
-  border: 1px solid white;
+  border: 1px solid rgba(43, 48, 59, 0.5);
+  background: linear-gradient(135deg, rgb(25, 29, 36), rgb(33, 36, 44));
+  border-radius: 10px;
+}
+
+.remove {
+  background-color: rgba(239, 67, 67, 0.1);
+  padding: 8px;
+  border-radius: 10px;
+}
+
+.muted {
+  color: rgb(152, 164, 179);
 }
 </style>
