c0ead32ac8407f5f92de81d90d9603d74a65a5c8
RondoYummy <basrers@ukr.net>
2025-11-16 06:58:20 +0000

    Update tapo name

--- python/tapo/tapo_routes.py
+++ python/tapo/tapo_routes.py
@@ -74,12 +74,15 @@ def turn_on_device(ip: str):
 
         update_data = {
             "device_on": True,
-            "name": name,
             "model": info.get("model"),
             "fw_ver": info.get("fw_ver"),
             "hw_ver": info.get("hw_ver"),
             "device_id": info.get("device_id"),
         }
+
+        if name:
+            update_data["name"] = name
+
         db.update_tapo_device_by_ip(ip, update_data)
         return {"status": "on", "ip": ip}
     except Exception as e:
@@ -99,12 +102,15 @@ def turn_off_device(ip: str):
 
         update_data = {
             "device_on": False,
-            "name": name,
             "model": info.get("model"),
             "fw_ver": info.get("fw_ver"),
             "hw_ver": info.get("hw_ver"),
             "device_id": info.get("device_id"),
         }
+
+        if name:
+            update_data["name"] = name
+
         db.update_tapo_device_by_ip(ip, update_data)
         return {"status": "off", "ip": ip}
     except Exception as e:
